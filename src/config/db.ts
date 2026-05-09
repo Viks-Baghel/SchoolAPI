38a24bc25ea55d9864c31eb9f1257faff68f4400
Vikas <vikasbaghel9837@gmail.com>
2026-05-09 13:01:19 +0530

    Fix DB

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,9 +4,13 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
+  port:     Number(process.env.DB_PORT),
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -19,7 +23,6 @@ const pool = mysql.createPool({
     conn.release();
   } catch (err: any) {
     console.error('MySQL connection failed:', err.message);
-    process.exit(1);
   }
 })();
 
